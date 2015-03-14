f931907aac9c2527d353b3122c7463cf1deb3b72
Calvin Chan <mail@calvinwchan.com>
2015-03-13 23:16:52 -0700

    url modifications

--- kalite/content/urls.py
+++ kalite/content/urls.py
@@ -7,4 +7,5 @@ topictree_resource = TopicTreeResource()
 
 urlpatterns = patterns(__package__ + '.views',
     url(r'^', 'content_view', {}, 'content_view'),
+    url(r'^api/', include(topictree_resource.urls)),
 )
